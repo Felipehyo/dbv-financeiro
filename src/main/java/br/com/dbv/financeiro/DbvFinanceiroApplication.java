c26bce1c6ffdf8403761f636d4e1f8089b2032d0
Felipe Pinheiro Dantas <felipehyo@gmail.com>
2022-09-27 18:09:21 -0300

    Initial commit

---
+++ src/main/java/br/com/dbv/financeiro/DbvFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbv.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbvFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbvFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dbv/financeiro/DbvFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbv.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbvFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
